893f7cb5d225998bcb59f543f98d841b9670ce3a
Nigel Hearn <heni1304@stcloudstate.edu>
2019-03-23 15:29:25 -0500

    SerialManager start

---
+++ ArduinoRFID/SerialManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ArduinoRFID
+{
+    sealed class SerialManager
+    {
+        #region Singleton Instantiation
+        static SerialManager() { }
+        private SerialManager() { }
+        public static SerialManager Instance { get; } = new SerialManager();
+        private static SerialPort _serialPort = new SerialPort();
+        #endregion
+
+        public string CurrentPortName
+        {
+            get { return _serialPort.PortName; }
+        }
+        
+    }
+}
